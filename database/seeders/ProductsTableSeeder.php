4ea765e2d01c3bde119eb61f172e00ed2a93e321
haruka01152 <haruka.0115maple@yahoo.ne.jp>
2021-05-25 16:56:58 +0900

    主要テーブルのシーディングを実行

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'maker_id' => 1,
+            'name' => 'ZX電話機',
+            'price' => 20000,
+            'quantity' => 10,
+            'last_editor' => 1,
+        ],
+        [
+            'maker_id' => 1,
+            'name' => 'NX電話機',
+            'price' => 25000,
+            'quantity' => 15,
+            'last_editor' => 2,
+        ],
+        [
+            'maker_id' => 2,
+            'name' => 'サクサ電話機',
+            'price' => 30000,
+            'quantity' => 13,
+            'last_editor' => 1,
+        ],
+        [
+            'maker_id' => 2,
+            'name' => 'サクサHUB',
+            'price' => 20000,
+            'quantity' => 10,
+            'last_editor' => 3,
+        ],
+        [
+            'maker_id' => 3,
+            'name' => 'セキュリティハウス　カメラ',
+            'price' => 15000,
+            'quantity' => 3,
+            'last_editor' => 2,
+        ],
+    );
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'hoge1',
+            'email' => 'hoge1@hoge.com',
+            'password' => Hash::make('password'),
+            'authority' => 0,
+        ],[
+            'name' => 'hoge2',
+            'email' => 'hoge2@hoge.com',
+            'password' => Hash::make('password'),
+            'authority' => 1,
+        ],[
+            'name' => 'hoge3',
+            'email' => 'hoge3@hoge.com',
+            'password' => Hash::make('password'),
+            'authority' => 2,
+        ],
+    );
+    }
+}
