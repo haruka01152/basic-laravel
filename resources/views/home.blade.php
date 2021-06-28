e68dd93c33f630eac58cf6c99437cd2f9fb86307
haruka01152 <haruka.0115maple@yahoo.ne.jp>
2021-06-28 17:57:30 +0900

    ホームの説明文を追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-@section('title', 'ホーム')
+    @section('title', 'ホーム')
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             Home
@@ -11,6 +11,21 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
             </div>
+
+            <div class="mt-24 bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+                <div class="border rounded-lg border-gray-400 p-10">
+                    <h3 class="text-lg text-blue-600 mb-5 pb-2 border-b-2 border-blue-600 border-dotted"><i class="fas fa-cubes fa-lg"></i>　{{Auth::user()->name}}さんの権限は　【{{Auth::user()->authorities->name}}】　です</h3>
+                    <p class="leading-6 pb-5 text-sm">こちらは社内在庫管理用システムです。<br>
+                        各ユーザーごとに権限が割り振られており、それに応じて出来る操作が異なります。<br>
+                        クリック権限のないボタンなどは押せなくなっていますので、あらかじめご了承ください。</p>
+
+                    <p class="leading-6 text-sm">
+                        【閲覧者】　在庫リスト・商品変更履歴の閲覧、在庫リストのダウンロードが可能です。<br>
+                        【編集者】　在庫リスト・商品変更履歴の閲覧/編集、在庫リストのダウンロードが可能です。<br>
+                        【管理者】　在庫リスト・商品変更履歴の閲覧/編集、在庫リストのダウンロード、ユーザー管理が可能です。</p>
+                </div>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
