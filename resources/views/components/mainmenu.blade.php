d7253337527a88120ca9f8a5513e22a3f20a5cf8
haruka01152 <haruka.0115maple@yahoo.ne.jp>
2021-10-04 15:02:45 +0900

    現在のページが分かるようデザイン変更

--- resources/views/components/mainmenu.blade.php
+++ resources/views/components/mainmenu.blade.php
@@ -1,9 +1,21 @@
+@php
+$menus = [
+'home' => 'ホーム',
+'index' => '在庫リスト',
+'log' => '商品変更履歴',
+'csv' => 'CSVダウンロード',
+'mypage' => 'マイページ',
+];
+@endphp
+
 <div class="bg-white border-t border-b border-solid border-gray-300 shadow-lg hidden sm:block">
-        <div class="container flex justify-between max-w-7xl mx-auto px-4 lg:px-8">
-            <a class="hover:bg-gray-200 inline-block py-3 w-48 text-center border-r border-l border-gray-300" href="{{route('home')}}">ホーム</a>
-            <a class="hover:bg-gray-200 inline-block py-3 w-48 text-center border-r border-l border-gray-300" href="{{route('index')}}">在庫リスト</a>
-            <a class="hover:bg-gray-200 inline-block py-3 w-48 text-center border-r border-l border-gray-300" href="{{route('log')}}">商品変更履歴</a>
-            <a class="hover:bg-gray-200 inline-block py-3 w-48 text-center border-r border-l border-gray-300" href="{{route('csv')}}">CSVダウンロード</a>
-            <a class="hover:bg-gray-200 inline-block py-3 w-48 text-center border-r border-l border-gray-300" href="{{route('mypage')}}">マイページ</a>
-        </div>
+    <div class="container flex justify-between max-w-7xl mx-auto px-4 lg:px-8">
+        @foreach($menus as $key => $value)
+        @if(\Route::currentRouteName() == $key)
+        <a class="hover:bg-blue-100 inline-block py-3 w-48 text-center border-r border-l border-b-4 border-blue-400" href="{{route($key)}}">{{$value}}</a>
+        @else
+        <a class="hover:bg-gray-200 inline-block py-3 w-48 text-center border-r border-l border-gray-300" href="{{route($key)}}">{{$value}}</a>
+        @endif
+        @endforeach
     </div>
+</div>
