2a899df477f5dee111fdb6a0a14a0b1bf4e0b6c5
haruka01152 <haruka.0115maple@yahoo.ne.jp>
2021-10-05 16:05:10 +0900

    メニューの色を変更

--- resources/views/components/mainmenu.blade.php
+++ resources/views/components/mainmenu.blade.php
@@ -12,7 +12,7 @@ $menus = [
     <div class="container flex justify-between max-w-7xl mx-auto px-4 lg:px-8">
         @foreach($menus as $key => $value)
         @if(\Route::currentRouteName() == $key)
-        <a class="hover:bg-blue-100 inline-block py-3 w-48 text-center border-r border-l border-b-4 border-gray-400" href="{{route($key)}}">{{$value}}</a>
+        <a class="hover:bg-gray-200 inline-block py-3 w-48 text-center border-r border-l border-b-4 border-gray-400" href="{{route($key)}}">{{$value}}</a>
         @else
         <a class="hover:bg-gray-200 inline-block py-3 w-48 text-center border-r border-l border-gray-300" href="{{route($key)}}">{{$value}}</a>
         @endif
